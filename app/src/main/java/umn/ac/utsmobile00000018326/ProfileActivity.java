bcbc1a7a968be1a3ba9a24f8134802ee93c76882
ladytheresa <ladytheresa65@gmail.com>
2020-03-21 11:53:13 +0700

    added creator profile

--- app/src/main/java/umn/ac/utsmobile00000018326/ProfileActivity.java
+++ app/src/main/java/umn/ac/utsmobile00000018326/ProfileActivity.java
@@ -3,12 +3,15 @@ package umn.ac.utsmobile00000018326;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+    ImageView pic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
     }
 }
